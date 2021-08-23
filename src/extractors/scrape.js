b88dbd729e868dca029d43dd38179a60a4d200c6
HyperCrowd <info@hypercrowd.org>
2021-08-23 19:25:51 -0400

    At least the damn thing is correctly figuring out when nothing is found

--- src/extractors/scrape.js
+++ src/extractors/scrape.js
@@ -1,14 +1,37 @@
 const playwright = require('playwright')
+const { webkit } = require('playwright')
 
 const scrapeBrowers = process.env.SCRAPE_BROWSERS || 'webkit'
 const browserTypes = scrapeBrowers.split(',')
+const headless = process.env.SCRAPE_HEADLESS === undefined
+  ? true // @TODO change to true
+  : process.env.SCRAPE_HEADLESS === '1'
 
 export async function scrape (url, onLoad) {
   for (const browserType of browserTypes) {
     try {
-      const browser = await playwright[browserType].launch()
-      const context = await browser.newContext()
+      const browser = await playwright[browserType].launch({ headless })
+      const context = await browser.newContext({
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        viewport: {
+          width: 1920,
+          height: 1200
+        }
+      })
       const page = await context.newPage()
+      page.setExtraHTTPHeaders({
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Thu, 15 Apr 2021 13:21:33 GMT",
+        "if-none-match": "\"56836141fa31d71:0\"",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "sec-gpc": "1",
+        "upgrade-insecure-requests": "1"
+      })
       await page.goto(url)
       await onLoad(page, browser)
       await browser.close()
--- src/sources/galveston-deeds.js
+++ src/sources/galveston-deeds.js
@@ -2,13 +2,12 @@ const { scrape } = require('../extractors/scrape')
 const moment = require('moment')
 
 const searchPortal = 'https://ava.fidlar.com/txgalveston/avaweb/#!/search'
-const searchResultsPortal = 'https://ava.fidlar.com/txgalveston/avaweb/#!/searchResults'
 const startDateSelector = '#StartDate'
 const endDateSelector = '#basicCriteria > div > div > div:nth-child(2) > div:nth-child(2) > div > input'
 const nameSelector = '#last-name'
 const searchButtonSelector = '#topFormButtons > button.btn.btn-custom.pull-right.btnMargin'
-const searchResultsLoadedSelector = '//*[@id="viewWrapper"]/div/section/div[1]/button[2]'
-const noResultsFoundSelector = '/html/body/div[3]/div/div/div/div/label'
+const searchResultsLoadedSelector = '#viewWrapper > div > section > div.input-block-level.btnContainer > button.btn.btn-custom.pull-right.btnMargin.ng-scope'
+const noResultsFoundSelector = 'body > div.modal.fade.ng-isolate-scope.in > div > div > div > span'
 
 export function search (startDate = new Date(), endDate = undefined, name = '') {
   const startDateFormatted = moment(startDate).format('YYYY-MM-DD')
@@ -17,10 +16,26 @@ export function search (startDate = new Date(), endDate = undefined, name = '') 
     : moment(endDate).format('YYYY-MM-DD')
 
   scrape(searchPortal, async (page, browser) => {
-    await page.on('crash', async msg => console.error(msg))
     await page.on('console', async msg => {
-      console.log(msg.text())
+      console.log('--Console:', msg.text())
       if (msg.text() === '[search]  Subdivisions loaded null') {
+
+        page.waitForNavigation().then(async () => {
+          // Move on to the next page
+          await page.screenshot({ path: `pass-${Date.now()}.png` })
+          page.waitForSelector(searchResultsLoadedSelector).then(() => {
+            console.log('found stuff')
+          })
+        })
+
+        page.waitForSelector(noResultsFoundSelector).then(async () => {
+          await page.screenshot({ path: `fail-${Date.now()}.png` })
+          console.log('nothing found')
+        })
+
+        console.log('waiting for things to be loaded')
+        await page.waitForSelector(startDateSelector)
+
         console.log('evaluate')
         await page.evaluate(async ({ startDateSelector, endDateSelector, nameSelector, searchButtonSelector, startDateFormatted, endDateFormatted, name }) => {
           $(startDateSelector).val(startDateFormatted)
@@ -28,21 +43,12 @@ export function search (startDate = new Date(), endDate = undefined, name = '') 
           $(nameSelector).val(name)
           $(searchButtonSelector).click()
         }, { startDateSelector, endDateSelector, nameSelector, searchButtonSelector, startDateFormatted, endDateFormatted, name })
-        
-        await page.waitForNavigation()
-
-        await page.evaluate(async () => {
-
-        })
-        console.log('screenshot')
-
-        await page.screenshot({ path: `example-${Date.now()}.png` })
-      }     
+      }
     })
 
-    await page.waitForElementState(searchButtonSelector, {
-      state: 'visible'
-    })
+    console.log('waiting...')
+    await page.waitForFunction(selector => true === false, startDateSelector)
+    // await page.waitForLoadState('networkidle')
   })
 }
 
